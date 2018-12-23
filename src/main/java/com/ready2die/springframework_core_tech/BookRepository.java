ec028bf8a1f71145f6b589a701f2205100d5edeb
Leesunghee <realwunz@gmail.com>
2018-12-23 22:11:58 +0900

    Environment profile

--- src/main/java/com/ready2die/springframework_core_tech/BookRepository.java
+++ src/main/java/com/ready2die/springframework_core_tech/BookRepository.java
@@ -1,8 +1,5 @@
 package com.ready2die.springframework_core_tech;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
 public interface BookRepository {
 
     Book save(Book book);
