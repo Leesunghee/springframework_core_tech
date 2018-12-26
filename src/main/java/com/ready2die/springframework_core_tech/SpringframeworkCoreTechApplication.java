dcd733211a00b9a4ffbc41ac07e98242d19c8e09
Leesunghee <realwunz@gmail.com>
2018-12-26 21:50:05 +0900

    spring AOP

--- src/main/java/com/ready2die/springframework_core_tech/SpringframeworkCoreTechApplication.java
+++ src/main/java/com/ready2die/springframework_core_tech/SpringframeworkCoreTechApplication.java
@@ -1,26 +1,10 @@
 package com.ready2die.springframework_core_tech;
 
-import com.ready2die.out.MyService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.scheduling.annotation.EnableAsync;
-
-import java.util.Arrays;
-import java.util.function.Supplier;
 
 @SpringBootApplication
 @PropertySource("classpath:/app.properties")
